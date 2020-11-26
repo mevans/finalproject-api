02f4f247ba813d89626c27270a9e94a4ebb44d59
Matthew Evans <matthew@evans99.co.uk>
2020-11-26 22:26:34 +0000

    feat(core): include custom models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import User, Patient, Doctor
+
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Doctor)
